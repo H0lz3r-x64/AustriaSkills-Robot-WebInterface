ed47df784f4e1bc7a3f44914b729b466bf304294
Jan Trummer <jan.trummer@knapp.com>
2023-08-17 12:57:12 +0200

    Changes

--- robot_comms.js
+++ robot_comms.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var lastLocation = [0, 0];
-const location_endpoint = 'http://localhost:3000/robot_location';
+const location_endpoint = 'http://10.12.34.2:3000/robot_location';
 
 function scaleValue(value, scale) {
     return value / scale;
@@ -13,7 +13,7 @@ function fetchData() {
         .then(data => {
             ctx.fillStyle = "red";
             
-            ctx.clearRect(lastLocation[0], lastLocation[1], 10, 10);
+            ctx.clearRect(lastLocation[0] - 1, lastLocation[1] - 1, 12, 12);
 
             ctx.fillRect(scaleValue(data.X, upScale), scaleValue(data.Y, upScale), 10, 10);
             
