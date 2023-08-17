d4b3df84b9d77e1d0efe819799523207a7a7a55a
Jan Trummer <jan.trummer@knapp.com>
2023-08-17 10:17:48 +0200

    Delete backend

---
+++ robot_comms.js
@@ -0,0 +1,27 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var lastLocation = [0, 0];
+const location_endpoint = 'http://localhost:3000/robot_location';
+
+function scaleValue(value, scale) {
+    return value / scale;
+}
+
+function fetchData() {
+    fetch(location_endpoint)
+        .then(response => response.json())
+        .then(data => {
+            ctx.fillStyle = "red";
+            
+            ctx.clearRect(lastLocation[0], lastLocation[1], 10, 10);
+
+            ctx.fillRect(scaleValue(data.X, upScale), scaleValue(data.Y, upScale), 10, 10);
+            
+            lastLocation = [scaleValue(data.X, upScale), scaleValue(data.Y, upScale)];
+        })
+        .catch(error => {
+            console.error("Error fetching data: ", error);
+        });
+}
+
+setInterval(fetchData, 500);
